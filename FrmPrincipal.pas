c87acd080eec1df1dcf54cbd687a80e4df146139
matheuzgit <42494194+matheuzgit@users.noreply.github.com>
2019-02-22 08:00:52 -0300

    Add files via upload

---
+++ FrmPrincipal.pas
@@ -0,0 +1,24 @@
+unit FrmPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormPrincipal = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormPrincipal: TFormPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ ProjVisual.dpr
@@ -0,0 +1,14 @@
+program ProjVisual;
+
+uses
+  Vcl.Forms,
+  FrmPrincipal in 'FrmPrincipal.pas' {FormPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.Run;
+end.
